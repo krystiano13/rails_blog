bb6412fbd20b2ea70e0a1e9f04195ec79f6f1346
krystiano13 <crystianotv@gmail.com>
2024-04-10 16:37:12 +0200

    create view and method for showing single blog post on page

--- app/controllers/concerns/blog_post_controller.rb
+++ app/controllers/concerns/blog_post_controller.rb
@@ -1,5 +1,13 @@
 class BlogPostController < ApplicationController
   def index
-    @blog_post = BlogPost.all
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+    @blog_post = BlogPost.find_by(id: params[:id])
+
+    if !@blog_post.present?
+      redirect_to(root_path)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   get "up" => "rails/health#show", as: :rails_health_check
+
   root "blog_post#index"
+  get "posts/:id", to: "blog_post#show"
+
 end
