7234401bbefe7df1c31370c7f86d0229f2c0e205
krystiano13 <crystianotv@gmail.com>
2024-04-11 13:18:07 +0200

    implement updating posts

--- app/controllers/concerns/blog_post_controller.rb
+++ app/controllers/concerns/blog_post_controller.rb
@@ -18,7 +18,7 @@ class BlogPostController < ApplicationController
   end
 
   def create
-    @blog_post = BlogPost.new(get_params())
+    @blog_post = BlogPost.new(get_params)
 
     if @blog_post.save
       redirect_to root_path
@@ -28,7 +28,22 @@ class BlogPostController < ApplicationController
   end
 
   def edit
+    @blog_post = BlogPost.find_by(id: params[:id])
 
+    if !@blog_post.present?
+      redirect_to posts_path
+    end
+  end
+
+  def update
+    @blog_post = BlogPost.find_by(id: params[:id])
+
+    if @blog_post.present?
+      @blog_post.update(get_params)
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "posts/new", to: "blog_post#new", as: :new_post
   get "posts/:id/edit", to: "blog_post#edit", as: :edit_post
   get "posts/:id", to: "blog_post#show", as: :posts
+  patch "posts/:id", to: "blog_post#update", as: :patch_post
   post "posts/new/create", to: "blog_post#create", as: :create_post
 end
