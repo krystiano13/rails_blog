e1af8bcac0a5f2d6a199b89adc8648a1754e7d19
krystiano13 <crystianotv@gmail.com>
2024-04-15 11:24:59 +0200

    write test for post scheduling

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? with no date returns true" do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? with date returns false" do
+    assert !BlogPost.new(published_at: DateTime.new).draft?
+  end
+
+  test "scheduled? with with future date returns true" do
+    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+  end
+
+  test "scheduled? with past date returns false" do
+    assert !BlogPost.new(published_at: 1.year.ago).scheduled?
+  end
+
+  test "published? with past date returns true" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? with future date returns false" do
+    assert !BlogPost.new(published_at: 1.year.from_now).published?
+  end
+
 end
